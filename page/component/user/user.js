49c95e9e647f4ead26a19e3ec434fb84dd82fc70
xuwenming <252429711@qq.com>
2017-09-28 16:09:46 +0800

    v0928

---
+++ page/component/user/user.js
@@ -0,0 +1,27 @@
+// page/component/user/user.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userName: '',
+    avatarUrl: '../../../image/default_icon.png',
+    shopName: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      userName: options.userName,
+      avatarUrl: options.avatarUrl || '../../../image/default_icon.png',
+      shopName: options.shopName
+    });
+  },
+
+  onPullDownRefresh: function () {
+    wx.stopPullDownRefresh()
+  }
+})
